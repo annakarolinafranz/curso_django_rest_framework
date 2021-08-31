5f2ba2b66316646b86084cb4c7007425274cd9fd
annakarolinafranz <nerdstecnologia0@gmail.com>
2021-08-31 00:42:40 -0300

    django_rest_framework

---
+++ README.md
@@ -0,0 +1,1 @@
+# curso_django_rest_framework
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/karolsdev/PycharmProjects/CursoDjangoFramework/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
